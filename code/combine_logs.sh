3296d1cc36960bc1b16e775549eccd0c9043d242
Markus Ankenbrand <markus@ankenbrand.me>
2020-07-20 12:11:18 +0000

    Add code and abstract

--- code/combine_logs.sh
+++ code/combine_logs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-(echo -n "run,"; head -n1 multi_balanced_cv0.csv) >combined_multi.csv
+(echo -n "run,"; head -n1 multi_ce_cv4.csv) >combined_multi.csv
 for i in multi*.csv; do BASE=$(basename $i .csv); grep -v epoch $i | perl -pe 's/^\d+,/'$BASE',$.,/'; done >>combined_multi.csv
 
-(echo -n "run,"; head -n1 lge_scarOnly.5_cv0.csv) >combined_lge.csv
+(echo -n "run,"; head -n1 lge_scarOnly.8_cv4.csv) >combined_lge.csv
 for i in lge*.csv; do BASE=$(basename $i .csv); grep -v epoch $i | perl -pe 's/^\d+,/'$BASE',$.,/'; done >>combined_lge.csv
 
-(echo -n "run,"; head -n1 t2_edemaOnly.5_cv0.csv) >combined_t2.csv
+(echo -n "run,"; head -n1 t2_edemaOnly.8_cv4.csv) >combined_t2.csv
 for i in t2*.csv; do BASE=$(basename $i .csv); grep -v epoch $i | perl -pe 's/^\d+,/'$BASE',$.,/'; done >>combined_t2.csv
